fde709395476dc50d79a83b4ce6a00fa9d9e7863
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-04-29 13:38:48 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import React from "react"
-// import styled from "styled-components"
+import React, { useState, useMemo } from "react"
+import styled from "styled-components"
 
-// const Grid = styled.div`
-//   display: grid;
-//   --color: #443c2e;
-// `
+const rowNum = 10
+const Body = styled.div`
+  --color: #d2a7a4;
+  --row-num: ${rowNum};
+  --w: 3em;
+`
 
-// const Cell = styled.div`
-//   border: 1px solid var(--color);
-//   height: 3em;
-//   width: 3em;
-// `
+const Outline1 = styled.div`
+  border: 2px var(--color) solid;
+`
+const Outline2 = styled.div`
+  border: 4px var(--color) double;
+  padding-right: 0.8em;
+  padding-left: 0.8em;
+`
+
+const Outline = ({ children }) => (
+  <Outline1>
+    <Outline2>{children}</Outline2>
+  </Outline1>
+)
+
+const Grid = styled.div`
+  display: grid;
+  grid-auto-columns: 3em;
+  grid-template-rows: repeat(var(--row-num), var(--w));
+  grid-auto-flow: column;
+  direction: rtl;
+  grid-column-gap: 1em;
+`
+
+const Cell = styled.div`
+  border: var(--color) 1px solid;
+  border-top: dotted var(--color) 1px;
+  border-bottom: 0px;
+  margin-top: -1px;
+  writing-mode: vertical-rl;
+  :nth-child(${rowNum}n + 1) {
+    border-top: 0px;
+    /* 1px solid var(--color); */
+  }
+  :nth-child(${rowNum}n) {
+    border-bottom: 0px;
+  }
+  line-height: 2em;
+  font-size: 22px;
+  text-align: center;
+  vertical-align: middle;
+  font-family: "ヒラギノ明朝 ProN W6", "HiraMinProN-W6", "HG明朝E",
+    "ＭＳ Ｐ明朝", "MS PMincho", "MS 明朝", serif;
+`
+
+const lorem =
+  "依然として速度を増しながら進むにつれて、夜と日中の切り替わりが曖昧になり、一続きの灰色になった。空は深い青色で夕暮れ時のような明るい光で照らされている。急に現れる太陽はきらめく円弧の形をした光の筋となった。月はおぼろげに揺らぐ帯となり、星は見えなかった。ただ、時折青い空に明るく瞬く円が見えた。"
+
+const TextArea = styled.textarea`
+  height: 5em;
+`
 
 function App() {
+  const [state, setState] = useState(lorem)
+  const chars = useMemo(() => {
+    const str = state.split("")
+    const r = rowNum - (str.length % rowNum)
+    return str.concat(Array.from({ length: r }))
+  }, [state])
   return (
-    <div>
-      <div>a</div>
-      {/* <Grid>
-        <Cell>あ</Cell>
-      </Grid> */}
-    </div>
+    <Body>
+      <div>
+        <TextArea onChange={(e) => setState(e.target.value)} value={state} />
+      </div>
+      <Outline>
+        <Grid>
+          {chars.map((c, key) => (
+            <Cell key={key}>{c}</Cell>
+          ))}
+        </Grid>
+      </Outline>
+    </Body>
   )
 }
 
