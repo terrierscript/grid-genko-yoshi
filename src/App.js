cd7682444f636a4106a15be7626454aa83f0f733
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-04-29 11:57:05 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+// import styled from "styled-components"
+
+// const Grid = styled.div`
+//   display: grid;
+//   --color: #443c2e;
+// `
+
+// const Cell = styled.div`
+//   border: 1px solid var(--color);
+//   height: 3em;
+//   width: 3em;
+// `
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>a</div>
+      {/* <Grid>
+        <Cell>あ</Cell>
+      </Grid> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
